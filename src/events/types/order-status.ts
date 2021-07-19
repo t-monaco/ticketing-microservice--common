9aaca1e014bf6998a1da07dfc0e281a30640fe52
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-07-19 11:13:56 -0300

    Updates

--- src/events/types/order-status.ts
+++ src/events/types/order-status.ts
@@ -14,5 +14,5 @@ export enum OrderStatus {
 
     // The order has reserved the ticket and the user has
     // provided payment successfully
-    Complete = 'complete',
+    Completed = 'completed',
 }
